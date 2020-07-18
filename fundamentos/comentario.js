c9a206ec60d925f210d9c94414ea3eea53077d86
mgbatista <maiara.27bueno@gmail.com>
2020-07-18 16:59:15 -0700

    variaveis, constantes, tipagem, tipos em JavaScript

---
+++ fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// Comentario de uma linha
+console.log('Linha1')
+
+/*
+Comentário de
+multiplas linhas
+*/
+console.log('linha2')
+
+/*
+*Comentario de
+*multiplas 
+*linhas
+*/
+console.log('linha3')
