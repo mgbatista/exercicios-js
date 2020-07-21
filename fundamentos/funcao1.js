cdb7cb23f2247847550961e1d14cb58e10a36701
mgbatista <maiara.27bueno@gmail.com>
2020-07-20 22:59:28 -0700

    Quase tudo e funcao, variaveis com var, variaveis com let, var em loop

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,18 @@
+// Função sem retorno
+function ImprimirSoma(a,b){
+    console.log(a + b)
+}
+
+ImprimirSoma(2, 3)
+ImprimirSoma(2)
+ImprimirSoma(2, 3, 4, 5, 6, 7, 8)
+ImprimirSoma()
+
+// Função com retorno
+function soma(a, b = 1) {
+    return a + b
+}
+console.log(soma(2, 3))
+console.log(soma(2))
+console.log(soma())
+
