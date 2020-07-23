8a04e895a8adaefa669b1f98d74ebad16183685d
mgbatista <maiara.27bueno@gmail.com>
2020-07-22 19:21:08 -0700

    Operadores: atribuicao, destructuring1, 2, 3, 4

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,18 @@
+const a = 7
+let b = 3
+
+b += a // atribuição aditiva, atribuindo o valor de a ao b (3+7 = 10 ou b = b + a)
+console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
+
