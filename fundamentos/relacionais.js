59e46cce57a095062f875f9e1cffd59e48c72225
mgbatista <maiara.27bueno@gmail.com>
2020-07-23 21:45:29 -0700

    operadores: aritmeticos logicos, relacionais, unarios

---
+++ fundamentos/relacionais.js
@@ -0,0 +1,17 @@
+console.log('01)', '1' == 1) // um = é atribuição, dois == é comparação
+console.log('01)', '1' === 1) // '01)' é uma string, sig. primeiro teste, segundo teste ...
+console.log('03)', '3' != 3)
+console.log('04)', '3' !== 3)
+
+console.log('05)', 3 < 2)
+console.log('06)', 3 > 2)
+console.log('07)', 3 <= 2)
+console.log('08)', 3 >= 2)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log('09)', d1 === d2)
+console.log('10)', d1 == d2)
+console.log('11)', d1.getTime() === d2.getTime())
+console.log('12)', undefined == null)
+console.log('13)', undefined === null)
