f485cc2e56a3fb3d946a28c0afb6edb11862c7c0
mgbatista <maiara.27bueno@gmail.com>
2020-09-09 15:31:21 -0700

    instalando deps e scripts

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -9,7 +9,7 @@ const menorSalario = (func, funcAtual) => {
 
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    // console.log(funcionarios)
 
     // mulher chinesa com menos salario?
     const func = funcionarios
