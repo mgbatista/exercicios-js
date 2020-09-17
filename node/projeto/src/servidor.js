ef892797005cad56b6639fd06b7202cfce309e49
mgbatista <maiara.27bueno@gmail.com>
2020-09-16 18:57:27 -0700

    projeto andamento

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,10 +2,23 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 }) // Aqui ele vai converter para JSON
+    res.send(bancoDeDados.getProdutos()) // Aqui ele vai converter para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
